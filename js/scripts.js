20f1ca4894665731a1eb0d6f4b0159ef7b952d72
Michele Anselmi <mikyxello@users.noreply.github.com>
2017-12-08 20:57:40 +0100

    TEST API

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,70 @@
 /* Documentation by http://www.filezigzag.com/api */
 
-/* TOKEN By @Mikyxello */
-var request_header = "\
-	POST /jobs HTTP/1.1\
-	Host: http://api.filezigzag.com/fzz.svc/\
-	token: d571f022-b3b1-4f65-8b2c-7233755a64a7\
-	Content-Type: application/json"
+/* Header
+POST /jobs HTTP/1.1
+Host: http://api.filezigzag.com/fzz.svc/
+token: <your API token here>
+Content-Type: application/json
+*/
 
-/* Example of convertion MP4 to AVI */
-var json_mp4toavi_sample = {               
-				"target":"avi", 
-				"category":"video",
-				"source":"https://www.filezigzag.com/119915/a9234dfe-02b9-4a0f-8664-bcd6fb218ba9/Components_GIF_gif.MP4",
-				"backurl":"",
-				"options": [{
-					"width":"",
-					"height":"200",
-					"bitrate": "200",
-					"audioquality":"8",
-					"framrate": "20",
-					"start":"00:00:00",
-					"end":"00:10:00",
-					"rotate":"0",
-					"flip":"horizontally", 
-					"watermarkimageurl":"",
-					"changeaudiourl":""
-                }]
+// URL = http://api.filezigzag.com/fzz.svc/convertfile
+/* 
+{
+	"target":"png", 
+	"category":"image",
+	"source":"https://www.filezigzag.com/2/ea6bc5d3-03ca-44d4-b524-afd374ef7636/avi_to_mp4.jpg"
 }
 
-/* Output Sample */
-var json_output_sample = {
-	"Convertfile": {
-		"id": "a68f5430-3235-4e2d-894f-652df8257cda"
-	}
+{
+  "Convertfile": {
+    "id": "a68f5430-3235-4e2d-894f-652df8257cda"
+  }
+}
+*/
+
+
+// URL = http://api.filezigzag.com/fzz.svc/Getfile
+/*
+{
+"id":"9162555c-5faf-4bdc-a075-18a13684fb0a"
 }
 
-/* METHOD POST */
-var url_request = "http://api.filezigzag.com/fzz.svc/Getfile";
-
-var json_get_sample = {
-	"id":"9162555c-5faf-4bdc-a075-18a13684fb0a"
+{
+  "Getfile": {
+    "Description": "Converted file avaliable",
+    "FileURL": "https://filezigzag.s3.amazonaws.com/2/d3480506-dced-45c4-8aec-2f0e7f72cfb4/Components_GIF_gif_MP4_mp4.avi?AWSAccessKeyId=AKIAJP4TWG7FXXCF5XAQ&Expires=1480517001&Signature=y0dj0DxDIM3Oxr5hxg50CrQBgKQ%3D",
+    "Status": "7"
+  }
 }
+Status 1: File Received , 2 Queue, 3 Processing, 4 Fail, 5 Lock, 6 Deleted, 7 Success, 8 Unknown Error
+*/
 
-var json_get_output_sample = {
-	"Getfile": {
-		"Description": "Converted file avaliable",
-		"FileURL": "https://filezigzag.s3.amazonaws.com/2/d3480506-dced-45c4-8aec-2f0e7f72cfb4/Components_GIF_gif_MP4_mp4.avi?AWSAccessKeyId=AKIAJP4TWG7FXXCF5XAQ&Expires=1480517001&Signature=y0dj0DxDIM3Oxr5hxg50CrQBgKQ%3D",
-		"Status": "7"
-	}
+function requestAjax() {
+	var obj = {
+		"target":"png",
+		"category":"image",
+		"source":"http://www.geektoolz.net/Content/Wallpapers/Single%20Screen/Mac-Leopard-Wallpaper.jpg"
+	};
+
+	var header = {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+		"token": "3ee1b804-a811-43c4-9300-0d72a10e0282-886ece8e-e9a4-4102-ae1f-4ad1e690693c",
+		"Host": "http://api.filezigzag.com/fzz.svc/",
+		"Content-Type": "application/json"
+	};
+
+	$.ajax({
+		type: "POST",
+		url: "http://api.filezigzag.com/fzz.svc/convertfile",
+		datatype: "json",
+		data: JSON.stringify(obj),
+		headers: header,
+		crossDomain : true,
+		xhrFields: {
+			withCredentials: true
+		},
+		success: function(res) {alert(res.responseText);},
+		error: function(res) {alert("Error " + res.status);}
+	});
 }
