e70c96caa51745554123068f20455d03aa0b60be
riccardo <ricciricciard@gmail.com>
2018-01-24 18:54:54 +0100

    aggiunti file mancanti nel precedente commit

---
+++ Logger/logger.js
@@ -0,0 +1,14 @@
+var amqp =  require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'logger';
+
+    ch.assertQueue(q, {durable: false});
+    ch.consume(q, function(msg) {
+	  console.log(" Ricevuta %s", msg.content.toString());
+}, {noAck: true});
+  });
+});
+
+
